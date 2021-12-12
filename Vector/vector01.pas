acd912ad195499ec890073eaa029a730f561133a
PPM5 Admin <autoinst578@gmail.com>
2021-12-12 13:59:53 +0700

    add .gitignore and update vector

--- Vector/vector01.pas
+++ Vector/vector01.pas
@@ -1,9 +1,10 @@
 var OldRng : byte;
 
-procedure _101			(	rng : byte;	(* Running info *)
-						var rst : byte;
-						var RUT : real;	(* Run time *)
-						var STC : real);(* Start count *)
+procedure _101			(var Rng : arr_16_byte;	(* Running info *)
+						 var rst : arr_16_byte;
+						 var Orn : arr_16_real;
+						 var Rut : arr_16_real;		(* Run time *)
+						 var Stc : arr_16_real);(* Start count *)
 						
 var i : byte;
 
@@ -12,25 +13,25 @@ begin
 	
 	(* RUN TIME COUNTER *)
 	
-	for i := 1 to 16 do if odd(Rng[i]) then RUT[i]:=RUT[i]+1;
+	for i := 1 to 16 do if odd(Rng[i]) then Rut[i]:=Rut[i]+1;
 	
 	(* START TIME COUNTER *)
-	for i := 1 to 16 do if Rng[i] > OldRng[i] then STC[i]:=STC[i]+1;
+	for i := 1 to 16 do if Rng[i] > Orn[i] then Stc[i]:=Stc[i]+1;
 	
 	(* RESET COUNTER *)
 	for i := 1 to 16 do
 	begin
 		if odd (Rst[i] then
 			begin
-			RUT[i]:=0;
-			STC[i]:=0;
+			Rut[i]:=0;
+			Stc[i]:=0;
 			end;
 		end;
 	
 	(* RESET COUNTER AND SAVE RNG-SIGNALS *)
 	for i := 1 to 16 do
 	begin
-		OldRng[i] := Rng[i];
+		Orn[i] := Rng[i];
 		Rst[i] := 0;
 	    end;
 end;
